24840f7a9756399525634ff3eeedf2151ff8b279
Sam <sam@malpass.me.uk>
2018-03-07 17:48:28 +0000

    Add variables

--- Code Generator/Code Generator.hpp
+++ Code Generator/Code Generator.hpp
@@ -4,3 +4,7 @@
 #include <memory>
 #include "../Syntax Tree/Syntax Tree.hpp"
 #include "../Optimizer/Optimizer.hpp"
+extern std::unique_ptr<Compiler::SyntaxTree> root;
+extern int returnVal;
+using namespace std;
+extern string optimized;
