db1b99f315a51253a7b563b810a6caeb20753dcf
Sam <sam@malpass.me.uk>
2018-03-07 17:53:11 +0000

    Add main function

--- Main.cpp
+++ Main.cpp
@@ -4,3 +4,17 @@
 #include "Syntax Tree/Syntax Tree.hpp"
 extern int yyparse();
 extern std::unique_ptr<Compiler::SyntaxTree> root;
+int main()
+{
+	int result = yyparse();
+	if(result == 0)
+	{
+		generate();
+		std::cout << "Compilation Successful" << std::endl;
+	}
+	else
+	{
+		std::cout << "Compilation Failed" << std::endl;
+	}
+	return result;
+}
