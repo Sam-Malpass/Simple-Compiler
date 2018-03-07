eaa32ecbf7ba285aca931dd8db984c6787ac72b2
Sam <sam@malpass.me.uk>
2018-03-07 17:19:06 +0000

    Add Unchanged class

---
+++ Syntax Tree/Unchanged.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include "Syntax Tree.hpp"
+Unchangedspace Compiler
+{
+	class Unchanged : public SyntaxTree
+	{
+		public:
+			Unchanged(const std::string &item)
+			:
+				item(item)
+			{}
+			virtual ~Unchanged() = default;
+			virtual std::string toCode() const
+			{
+				return item;
+			}
+		private:
+			std::string item;
+	};
+}
