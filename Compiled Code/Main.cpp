0c2552665b051412de1af0714c28277696477c49
Sam <sam@malpass.me.uk>
2018-03-07 18:30:10 +0000

    Add the compiled output

---
+++ Compiled Code/Main.cpp
@@ -0,0 +1,12 @@
+/*C++ Code Generated Using SamC to C++ Compiler by Sam Malpass*/
+#include <iostream>
+int main()
+{
+int a=3; 
+int b=4; 
+int c=a+b; 
+std::cout << c; 
+return c; 
+
+}
+/*Compile C++ Code Yourself*/
